b7dfccfa8b89a182c9ff0738a6253d2dff754d81
Войткович Екатерина <115071542+Voitkovich@users.noreply.github.com>
2023-09-17 14:21:49 +0300

    Update HW_1.py

--- seminars/HWs/HW_1.py
+++ seminars/HWs/HW_1.py
@@ -3,17 +3,16 @@
 # если команда успешно выполнена и текст найден в её выводе и 
 # False в противном случае. Передаваться должна только одна 
 # строка, разбиение вывода использовать не нужно.
-
 import subprocess
 
 def check_command_output(command, text):
     try:
-        output = subprocess.check_output(command, shell=True, universal_newlines=True)
+        output = subprocess.run('cat /etc/os-release', shell=True, stdout=subprocess.PIPE, encoding='utf-8')
         return text in output
     except (subprocess.CalledProcessError, FileNotFoundError):
         return False
 
-command = "cd"
-text = "file.txt"
-result = check_command_output(command, text)
-print(result)
+    command = "cd"
+    text = "jammy"
+    result = check_command_output(command, text)
+    print(result)
